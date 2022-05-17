fa605ee2546b83fefa7b56138bb0a8fe4a9ab282
WillyRod <100033924+WillyRod@users.noreply.github.com>
2022-05-16 19:34:23 -0500

    AWS Terraform
    AWS Terraform

---
+++ 3 - Terraform EC2/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.13.0"
+    }
+  }
+}
+
+
+provider "aws" {
+	region = var.region
+	access_key = var.my_access_key
+	secret_key = var.my_secret_key
+}
+
+data "aws_ami" "amazon" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220426.0-x86_64-gp2"]
+  }
+  owners = ["137112412989"] # Canonical
+
+}
+
+resource "aws_instance" "example" {
+    ami = data.aws_ami.amazon.id
+	instance_type = "t2.micro"
+
+    ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = 8
+    volume_type = "gp2"
+    }
+    tags = {
+        Name = "EC2 PRUEBA TECNICA BOCC"
+    }
+    vpc_security_group_ids = [aws_security_group.instance.id]
+}
+
+resource "aws_security_group" "instance" {
+	name = "terraform-tcp-security-group"
+
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
---
+++ 3 - Terraform EC2/terraform.tfvars
@@ -0,0 +1,2 @@
+my_access_key = "AKIAWZREP43VOIQIUDNW"
+my_secret_key = "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"
